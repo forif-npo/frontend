08e3c0b51c33530123a18f3c2c675546d81a69b9
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2025-05-30 05:29:16 +0000

    feat(web): add i18n to signin page

--- apps/web/src/features/auth/signin/member-tab.tsx
+++ apps/web/src/features/auth/signin/member-tab.tsx
@@ -1,14 +1,13 @@
 import { InformationIcon } from "@repo/assets/icons/krds";
 import { Button } from "@ui/components/client";
 import { Body, InfoBox, Link } from "@ui/components/server";
+import { useTranslations } from "next-intl";
 
 export default function MemberLogin() {
+  const t = useTranslations("SignInPage.member-signin");
   return (
     <div className="mt-10 flex flex-col gap-10">
-      <Body className="text-text-basic">
-        부원 회원을 위한 통합 로그인입니다. 로그인을 하시면 보다 더 많은 정보와
-        서비스를 이용하실 수 있습니다.
-      </Body>
+      <Body className="text-text-basic">{t("description")}</Body>
       <div className="border-divider-gray-light rounded-3 flex flex-col gap-6 border px-10 py-8 shadow">
         <Button>한양대학교 이메일로 로그인</Button>
         <Button variant="text">회원가입</Button>
@@ -21,23 +20,20 @@ export default function MemberLogin() {
             />
           </span>
           <Body size="s" className="text-text-basic">
-            회칙 2장 제4조(자격과 구성)에 의거하여 부원 가입대상을 한양대학교
-            재·휴·졸업생으로 한정함에 따라 한양대학교 이메일을 통한
-            로그인/회원가입을 진행하고 있습니다. 아직 한양메일을 만드시지
-            않았다면{" "}
+            {t("info_section.text_0")}{" "}
             <Link
               size="s"
               href="https://hanyang.ac.kr"
               className="text-text-primary"
             >
-              다음 링크
-            </Link>
-            를 따라 만드실 수 있습니다.
+              {t("info_section.text_1")}
+            </Link>{" "}
+            {t("info_section.text_2")}
           </Body>
         </div>
       </div>
       <InfoBox
-        title="로그인에 어려움이 있으신가요?"
+        title={t("help_section.title")}
         variant="information"
         content={<InfoBoxContent />}
       />
@@ -46,24 +42,24 @@ export default function MemberLogin() {
 }
 
 const InfoBoxContent = () => {
+  const t = useTranslations("SignInPage.member-signin");
   return (
     <ul className="list-inside list-disc">
       <li className="text-text-subtle">
-        로그인{" "}
         <Link size="m" href="" className="underline underline-offset-2">
-          관련 도움말
-        </Link>
-        이나 다른 사용자가{" "}
+          {t("help_section.text_0")}
+        </Link>{" "}
+        {t("help_section.text_1")}{" "}
         <Link size="m" href="" className="underline underline-offset-2">
-          자주 묻는 질문
-        </Link>
-        을 확인해보세요.
+          {t("help_section.text_2")}
+        </Link>{" "}
+        {t("help_section.text_3")}
       </li>
       <li className="text-text-subtle">
         <Link size="m" href="" className="underline underline-offset-2">
-          카카오톡 오픈 채널
-        </Link>
-        로 연락주세요. 서비스에 로그인할 수 있도록 도와드리겠습니다.
+          {t("help_section.text_4")}
+        </Link>{" "}
+        {t("help_section.text_5")}
       </li>
     </ul>
   );
