cb22ecc1b34f97a15c3f9e78290a6658cc8b0088
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2025-08-24 15:47:50 +0900

    🚀(web): enable msw, remove next-theme
    use msw to mock apis. remove unused theme

---
+++ apps/web/src/mocks/MSWProvider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect } from "react";
+import { initMocks } from "./setup";
+
+export function MSWProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    initMocks();
+  }, []);
+  return <>{children}</>;
+}
---
+++ apps/web/src/mocks/handlers.ts
@@ -0,0 +1,9 @@
+import * as apis from "@/mocks/apis";
+import { http, passthrough, RequestHandler } from "msw";
+
+export const handlers: RequestHandler[] = [
+  http.get(/^\/_next\//, () => passthrough()),
+  http.get(/\/favicon\.ico$/, () => passthrough()),
+  http.get(/\/icon(-\d+x\d+)?\.png$/, () => passthrough()),
+  ...Object.values(apis),
+];
