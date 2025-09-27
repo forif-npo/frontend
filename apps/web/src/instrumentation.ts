80fed947e67821dc299b69543565c3a2601e4850
Jun Seong Pyo <standardstar@hanyang.ac.kr>
2025-09-27 13:44:04 +0900

    🐛(web): add await func in instrumentation

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -1,6 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { initMocks } = await import("@/mocks/setup");
-    initMocks();
+    await initMocks();
   }
 }
