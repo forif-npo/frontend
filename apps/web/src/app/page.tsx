c90184ef0d3775e9ce55ec4186c86c680482f1d2
donghee290 <eehd1127@naver.com>
2026-08-01 04:01:37 +0900

    🚀(web): 안내 부분 스터디 목록으로 라우트 변경

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -44,8 +44,8 @@ export default async function Page() {
           <CriticalAlert
             variant="information"
             link="/studies/list"
-            text="스터디 신청 절차를 자세히 알고 싶으신가요?"
-            title="스터디 신청 절차 안내 페이지로 이동"
+            text="매 학기 어떤 스터디가 열리는지 궁금하신가요?"
+            title="스터디 목록으로 이동"
             detailText="자세히 보기"
             className="shadow-sm"
           />
